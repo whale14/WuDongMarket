567dffa1b503a5cbcaf817f5e757c73cf2fd0810
whale14 <zzazzu14@gmail.com>
2022-05-03 11:33:49 +0900

    user 는 낄곳이아니지

--- UDonMarket/src/com/udon/mybatis/DBService.java
+++ UDonMarket/src/com/udon/mybatis/DBService.java
@@ -7,19 +7,11 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-// MyBatis 사용해서 작업할 SqlSession 을 만들어 줄
-// SqlSessionFactory 객체 생성을 위한 클래스
 public class DBService {
 	private static SqlSessionFactory factory;
 	
-	// static 초기화 구문
 	static {
 		try {
-			/*
-			SqlSessionFactoryBuilder factoryBuilder = new SqlSessionFactoryBuilder();
-			Reader reader = Resources.getResourceAsReader("com/mystudy/mybatis/config.xml");
-			factory = factoryBuilder.build(reader);
-			*/
 			factory = new SqlSessionFactoryBuilder()
 					.build(Resources.getResourceAsReader("com/udon/mybatis/config.xml"));	
 		} catch (IOException e) {
